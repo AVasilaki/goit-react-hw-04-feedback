73fc66b91cfad98c0f77644c10ad345d713d89d7
AVasilaki <poseydon463@gmail.com>
2023-10-27 15:35:46 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,8 @@
-// import { Component } from 'react';
 import { Feedback } from './feedback/Feedback';
 import { Statistic } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { useState } from 'react';
 
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   leaveFeedback = newState => {
-//     this.setState(prevState => ({
-//       [newState]: prevState[newState] + 1,
-//     }));
-//   };
-
-//   countTotalFeedback = total =>
-//     (total = this.state.good + this.state.neutral + this.state.bad);
-
-//   countPositiveFeedbackPercentage = percentege => {
-//     const state = this.state;
-//     const total = state.good + state.neutral + state.bad;
-
-//     if (total !== 0) {
-//       return Math.ceil((percentege = (state.good / total) * 100));
-//     }
-//   };
-
-//   render() {
-//     const state = this.state;
-
-//     return (
-//       <div>
-//         <Section title={'Please leave feedback'}>
-//           <Feedback leaveFeedback={this.leaveFeedback}></Feedback>
-//         </Section>
-//         <Section title={'Statistic'}>
-//           <Statistic
-//             state={state}
-//             countTotalFeedback={this.countTotalFeedback}
-//             countPositiveFeedbackPercentage={
-//               this.countPositiveFeedbackPercentage
-//             }
-//           ></Statistic>
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
